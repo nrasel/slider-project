1aa4b91ab01c4a0e704394dc5d08931f19ee267a
nrasel <nrasel452@gmail.com>
2021-07-23 12:25:54 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin:15,
+        items: 3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsiveClass: true,
+        responsiveRate: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    });
+});
